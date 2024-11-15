adad2c27517768ad3b1f307691e422ebc8f755aa
Rusakov Ivan <clutchthetec@gmail.com>
2024-11-15 17:14:25 +0300

    fix deleting when searching

--- src/NotesManager.js
+++ src/NotesManager.js
@@ -50,6 +50,6 @@ export const createEmptyNote = () => {
 export const search = (notes, text) => {
   if (text === "") return notes;
   return notes.filter(note => {
-    return (note.header.includes(text) || note.text.includes(text))
+    return (note.header.toLowerCase().includes(text.toLowerCase()) || note.text.toLowerCase().includes(text.toLowerCase()))
   })
 }
--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -3,6 +3,7 @@ import AddNoteButton from "./AddNoteButton";
 import SearchField from "./SearchField";
 import { search } from "../NotesManager";
 import { useState } from "react";
+import { deleteNote } from "../NotesManager";
 
 const NotesList = (props) => {
   const [notesToDisplay, setNotesToDisplay] = useState(props.notes);
@@ -11,13 +12,18 @@ const NotesList = (props) => {
     setNotesToDisplay(search(props.notes, text))
   }
 
+  const deletePressed = (id) => {
+    setNotesToDisplay(deleteNote(notesToDisplay, id));
+    props.deletePressed(id);
+  }
+
   return (
     <div className="noteslist container-xl">
       <SearchField searchEntered={searchEntered}></SearchField>
       <div className="row g-3">
         {notesToDisplay.map((note) => (
           <div className="col-sm-12 col-md-6 col-lg-4 col-xxl-3" onClick={() => props.notePressed(note)}>
-            <Note note={note} deletePressed={props.deletePressed}/>
+            <Note note={note} deletePressed={deletePressed}/>
           </div>
         ))}
         <div className="col-sm-12 col-md-6 col-lg-4 col-xxl-3" onClick={() => props.notePressed(null)}>
